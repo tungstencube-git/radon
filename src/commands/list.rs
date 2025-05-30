38796955aadf3deecfedbdec44d4016b203c7565
tungsten <mammadliyusif385@gmail.com>
2025-05-30 19:29:55 +0400

    Create list.rs

---
+++ src/commands/list.rs
@@ -0,0 +1,14 @@
+use crate::utils::get_installed_packages;
+use ansi_term::Colour::Green;
+
+pub fn list() {
+    println!("{}", Green.paint("Installed packages:"));
+    let packages = get_installed_packages();
+    if packages.is_empty() {
+        println!("No packages installed");
+    } else {
+        for pkg in packages {
+            println!("- {}", pkg);
+        }
+    }
+}
