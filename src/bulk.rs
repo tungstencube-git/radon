19b6d5f1db341658a431f4c1f2fc07481c25e5c0
tungsten <mammadliyusif385@gmail.com>
2025-06-14 06:40:03 +0000

    Delete src/bulk.rs

--- src/bulk.rs
+++
@@ -1,9 +0,0 @@
-use crate::commands::install;
-use ansi_term::Colour::Green;
-
-pub fn bulk_install(packages: &[String], flags: &[String]) {
-    for package in packages {
-        println!("{}", Green.paint(&format!("Installing {}...", package)));
-        install::install(package, None, false, None, None, flags);
-    }
-}
